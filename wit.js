3b11d3538d7a46324c8db8477382ebbc6dd9b5ba
cdsurfer0212 <cdsurfer0212@gmail.com>
2016-07-26 21:40:13 +0800

    integrate nba

--- wit.js
+++ wit.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let nba = require('nba');
 let Wit = require('node-wit').Wit;
 
 const accessToken = (() => {
@@ -20,6 +21,30 @@ const actions = {
       return resolve();
     });
   },
+  getPlayerInfo({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      var player = getFirstEntityValue(entities, 'player');
+      if (player) {
+        var mock = {playerId: 201144, season: '2013-14'};
+        nba.api.playerInfo(mock, function(err, response) {
+          var playerInfo = response.commonPlayerInfo[0];
+          context.team = playerInfo.teamCity + " " + playerInfo.teamName;
+        });
+
+      } else {
+        context.team = "I can't find the player";
+      }
+      return resolve(context);
+    });
+  }
+};
+
+const getFirstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] && Array.isArray(entities[entity]) && entities[entity].length > 0 && entities[entity][0].value;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
 };
 
 const wit = new Wit({accessToken, actions});
